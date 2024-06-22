58f7b08d84a98edbbdc802537b8e8593ec45e71e
hssaka7 <hssaka7@gmail.com>
2024-06-22 16:01:40 -0500

    working version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mltool',
     version='0.1.0',
-    packages=['mltool'],
+    packages=find_packages(),
     install_requires=[
         'graphviz',
         'matplotlib',
